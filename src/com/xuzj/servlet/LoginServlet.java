0c1b43f30e3e59b0d40e34c99b86348f4bf0d50a
superxuzj <271652260@qq.com>
2016-09-09 10:03:23 +0800

    first commit 过滤器

---
+++ src/com/xuzj/servlet/LoginServlet.java
@@ -0,0 +1,54 @@
+package com.xuzj.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println(username);
+		if("admin".equals(username)){
+			request.setAttribute("username", username);
+			
+			//验证用户是否登录 用户名放在session里面
+			HttpSession session =  request.getSession();
+			session.setAttribute("username", username);
+			System.out.println(username+" -- "+password);
+			response.sendRedirect(request.getContextPath()+"/succes.jsp");
+			
+		}else{
+			response.sendRedirect(request.getContextPath()+"/fail.jsp");
+		}
+	}
+
+}
